f022779a54b9075b35e00b506ad5a4752cb64802
Andres Cordoba <ancor1369@gmail.com>
2019-03-22 22:07:00 -0400

    This advancement starts the creation of the management page needed to relate the products with its corresponding stores, Multiple view of data will be obtained from here.

---
+++ scripts/ProductToStore/create.js
@@ -0,0 +1,26 @@
+
+var modal = document.getElementById('PrdStoreModal');
+modal.style.display = "block";
+
+var storeName = document.getElementById('txtPrdStorNameModal');
+storeName.innerText = "Name";
+
+var storea = document.getElementById('txtPrdStrNumberModal');
+storea.innerText = "Staples 266";
+
+var storeb = document.getElementById('txtPrdStrLoModal');
+storeb.innerText = "Staples 26644";
+
+// var table = document.getElementById('tblprdInStore');
+// let newRow = table.insertRow(-1);
+
+// let newCell = newRow.insertCell(0);
+// let secondCell = newRow.insertCell(1);
+// let thirdCell = newRow.insertCell(2);
+
+// let newText = document.createTextNode('Hola Producto nuevo');
+// let newText1 = document.createTextNode('este es un product');
+// let newText2 = document.createTextNode('https://bit.ly/jjdiw');
+// newCell.appendChild(newText);
+// secondCell.appendChild(newText1);
+// thirdCell.appendChild(newText2);
